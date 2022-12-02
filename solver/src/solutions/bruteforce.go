a532360aa70c38c3f87590fb0fd64a16a644e1fd
Adam Piaseczny <cypis183@gmail.com>
2022-12-02 12:46:15 +0100

    feat: removed the first edge from the bruteforce algorithm (isomorphism of solutions)

--- solver/src/solutions/bruteforce.go
+++ solver/src/solutions/bruteforce.go
@@ -19,8 +19,8 @@ func bruteforce(logger *log.Logger, graph g.Graph) int {
 
 	// Create the permutation generator instance
 	vertices := len(graph.Distances)
-	permGenerator := combin.NewPermutationGenerator(vertices, vertices)
-	permutation := make([]int, vertices)
+	permGenerator := combin.NewPermutationGenerator(vertices-1, vertices-1)
+	permutation := make([]int, vertices-1)
 	min := math.MaxInt
 
 	logger.Infof("Generated the permutation object")
@@ -30,11 +30,12 @@ func bruteforce(logger *log.Logger, graph g.Graph) int {
 		permGenerator.Permutation(permutation)
 		var total int
 		// Add the distance between the first element and the last element of the permutation
-		total += graph.Distances[permutation[0]][permutation[len(graph.Distances)-1]]
+		total += graph.Distances[0][permutation[len(permutation)-1]+1]
+		total += graph.Distances[0][permutation[0]+1]
 
 		// Add subsequent elements of the slice
 		for ind := 0; ind < len(permutation)-1; ind++ {
-			total += graph.Distances[permutation[ind]][permutation[ind+1]]
+			total += graph.Distances[permutation[ind]+1][permutation[ind+1]+1]
 		}
 
 		// Make it the new minimum if it is the best distance
